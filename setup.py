37a1d944178b62012038a27dcf159c5ed2be2f8a
EA264728 <elouan.argouarch@cea.fr>
2022-03-23 10:14:35 +0100

    change install_requires in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=['models_em'],
     url='',
     license='',
-    install_requires = ['torch', 'tqdm', 'seaborn'],
-    author='Elouan',
+    install_requires = ['torch', 'tqdm'],
+    author='Elouan ARGOUARCH',
     setup_requires = ['wheel'],
     author_email='elouan.argouarch@gmail.com',
     description=''
